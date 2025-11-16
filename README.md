9ad664d8c64f168f76dfe195c6ec1977c9c005bf
Brandon <146174490+branzyy@users.noreply.github.com>
2025-11-16 20:53:49 +0300

    Remove old PHP files, keep only Android app and TODO

--- README.md
+++
@@ -1,1 +0,0 @@
-# MyTermProject-API
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-
-header("Location: forms/loginform.php"); // Redirect to login page
-exit;
-?>
